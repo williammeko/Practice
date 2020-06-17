62d1b74a74a33015a49c2a37af60794e31ff1981
will <will@wmb.local>
2020-06-17 09:42:25 +0800

    [mqtt] move mqtt to helloworld project

--- mqtt/README.md
+++
@@ -1,6 +0,0 @@
-#### limitations/restrictions of mqtt
-
-[reference - ibm](https://www.ibm.com/support/knowledgecenter/SSQP8H/iot/platform/reference/mqtt/restrictions_limitations.html)
-
- - 5000 messages buffer size. Oldest messages are discarded when buffer is full.
- - The maximum payload size on Platform Service is 131072 bytes. 
--- mqtt/test/invoke.ps1
+++
@@ -1,8 +0,0 @@
-Invoke-RestMethod http://localhost:8081/publish -Method Post -ContentType application/json -Body '{"a":1,"b":3}'
-Invoke-RestMethod http://localhost:8081/send/topic/topic01 -Method Post -ContentType application/json -Body '{"topic":"topic01","msg":"msg02"}'
-
-0.99 | foreach {
-    Invoke-RestMethod http://localhost:8081/publish -Method Post -ContentType application/json -Body '{"a":1,"b":3}'
-}
-
-curl http://localhost:8081/publish -XPOST -H 'Content-Type: application/json' -d '{"a":1,"b":3}' -i
